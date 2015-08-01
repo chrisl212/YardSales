f137e487355f6094bb4df66af1cc2f7b2cf35984
Christopher Loonam <christopherloonam@ChristosMacBook.home>
2015-08-01 12:02:53 -0400

    Initial commit

---
+++ YardSales/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  YardSales
+//
+//  Created by Christopher Loonam on 8/1/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
